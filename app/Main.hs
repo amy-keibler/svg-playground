c406a52fc64d2a0b95f3313bdd989b6380eda53d
Jane Keibler <wanderingelf14@gmail.com>
2017-02-06 08:57:50 -0500

    quick transformation to individual files
    engine needed files instead of a single spritesheet

--- app/Main.hs
+++ app/Main.hs
@@ -4,22 +4,15 @@ module Main where
 import Animation
 
 import Graphics.Svg hiding (translate)
-import Data.Text hiding (foldr1, index)
+import Data.Text hiding (foldr1, index, zip, map)
 
-import Data.List (zipWith5)
+import Data.List (zip4)
 
 frameWidth :: Int
 frameWidth = 64
 
 numFrames :: Int
 numFrames = 10
-
-frameLines :: Int -> Line -> Line -> Line -> Line -> (Line, Line, Line, Line)
-frameLines index line0 line1 line2 line3 = (translate frameStart line0,
-                                            translate frameStart line1,
-                                            translate frameStart line2,
-                                            translate frameStart line3)
-  where frameStart = Point (index * frameWidth) 0
 
 framedLinesToSvg :: (Line, Line, Line, Line) -> Element
 framedLinesToSvg (line0, line1, line2, line3) = thickLineSvg line0
@@ -35,32 +28,29 @@ main = do
   let topForwardSlash = linearAnimateLine numFrames (Line (Point 20 44) (Point 44 20)) (Line (Point 38 44) (Point 38 20))
   let bottomBackSlash = linearAnimateLine numFrames (Line (Point 16 16) (Point 48 48)) (Line (Point 26 18) (Point 26 46))
   let bottomForwardSlash = linearAnimateLine numFrames (Line (Point 16 48) (Point 48 16)) (Line (Point 38 46) (Point 38 18))
-  let lineFrames = foldr1 (<>) $ framedLinesToSvg <$> zipWith5 frameLines [0 ..] bottomBackSlash bottomForwardSlash topBackSlash topForwardSlash
-  let file = svg (frames <> lineFrames)
-  writeFile "/tmp/spritesheet.svg" $ show file
+  let lineFrames = fmap (show . svg . (defaultFrame <> )) $ framedLinesToSvg <$> zip4 bottomBackSlash bottomForwardSlash topBackSlash topForwardSlash
+  let indexedFiles = zip [0..] lineFrames
+  mapM_ (\(index, file) -> writeFile ("/tmp/spritesheet_" ++ (show index) ++ ".svg") file) indexedFiles
 
 svg :: Element -> Element
 svg content = doctype <> with (svg11_ content) [Version_ <<- "1.1",
-                                                Width_ <<- pack (show (frameWidth * numFrames)),
-                                                Height_ <<- "64"]
+                                                Width_ <<- frameSize,
+                                                Height_ <<- frameSize]
+              where frameSize = pack $ show frameWidth
 
-defaultFrame :: Int -> Element
-defaultFrame index = rect_ [X_ <<- xPos 2, Y_ <<- "2",
+defaultFrame :: Element
+defaultFrame = rect_ [X_ <<- "2", Y_ <<- "2",
                             Width_ <<- paddedFrame 2, Height_ <<- paddedFrame 2,
                             "#660000" ->> Fill_, Fill_opacity_ <<- "0.5"]
-                     <> rect_ [X_ <<- xPos 4, Y_ <<- "4",
+                     <> rect_ [X_ <<- "4", Y_ <<- "4",
                                Width_ <<- paddedFrame 4, Height_ <<- paddedFrame 4,
                                "#990000" ->> Stroke_, Stroke_width_ <<- "8",
                                Fill_opacity_ <<- "0"]
-                     <> rect_ [X_ <<- xPos 2, Y_ <<- "2",
+                     <> rect_ [X_ <<- "2", Y_ <<- "2",
                                Width_ <<- paddedFrame 2, Height_ <<- paddedFrame 2,
                                "#cc0000" ->> Stroke_, Stroke_width_ <<- "4",
                                Fill_opacity_ <<- "0"]
-  where xPos padding = (pack .show) ((index * frameWidth) + padding)
-        paddedFrame padding = (pack . show) (frameWidth - (2 * padding))
-
-frames :: Element
-frames = foldr1 (<>) $ fmap defaultFrame [0 .. numFrames]
+  where paddedFrame padding = (pack . show) (frameWidth - (2 * padding))
 
 lineToSvg :: Text -> Text -> Line -> Element
 lineToSvg width color(Line (Point xS yS) (Point xE yE)) = line_ [X1_ <<- pack (show xS), Y1_ <<- pack (show yS),
